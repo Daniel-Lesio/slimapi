c3755a240f992ac16427a6287df1d05bbeb22524
Daniel Lesio <lesiewiczsales@gmail.com>
2019-06-27 12:14:14 +0200

    complete - without auth

--- src/config/db.php
+++ src/config/db.php
@@ -8,7 +8,7 @@ class db {
 
     public function connect(){
         $mysql_connect_str = "mysql:host=$this->dbhost;dbname=$this->dbname";
-        $dbConnection = new PDO(mysql_connect_str,$this->dbuser,$this->dbpass);
+        $dbConnection = new PDO($mysql_connect_str,$this->dbuser,$this->dbpass);
         $dbConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $dbConnection;
     }
